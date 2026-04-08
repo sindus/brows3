12d74165ba99a301d02ebcc21f20c36c3282d37e
Chan RG <rgchandrasekaraa@gmail.com>
2026-04-08 11:21:59 +1000

    fix(ci): stop release workflow loops

--- .github/scripts/extract-release-notes.js
+++ .github/scripts/extract-release-notes.js
@@ -3,13 +3,14 @@ const path = require('path');
 
 const changelogPath = path.join(process.cwd(), 'CHANGELOG.md');
 const version = require(path.join(process.cwd(), 'package.json')).version;
+const escapedVersion = version.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 
 try {
   const content = fs.readFileSync(changelogPath, 'utf8');
   
   // Regex to match the current version section
   // Matches: ## [x.x.x] - date ... content ... until next ## [x.x.x] or eof
-  const regex = new RegExp(`## \\[${version}\\] - \\d{4}-\\d{2}-\\d{2}([\\s\\S]*?)(?=## \\[|$)`, 'i');
+  const regex = new RegExp(`## \\[${escapedVersion}\\] - \\d{4}-\\d{2}-\\d{2}([\\s\\S]*?)(?=## \\[|$)`, 'i');
   
   const match = content.match(regex);
   
